abd1d060a756c59aaa9f2283acc71c32d0f5f779
Darya <darya.lapata@gmail.com>
2022-01-15 08:42:29 +0100

    fixed intendation

--- backend/server.js
+++ backend/server.js
@@ -9,28 +9,28 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 mongoose.Promise = Promise
 
 const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	accessToken: {
-		type: String,
-		default: () => crypto.randomBytes(128).toString('hex'),
-	},
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+  },
 });
 
 const User = mongoose.model('User', UserSchema);
 
 const ThoughtSchema = new mongoose.Schema({
-	message: {
-		type: String,
-		required: true,
-	},
+  message: {
+    type: String,
+    required: true,
+  },
 });
 
 const Thought = mongoose.model('Thought', ThoughtSchema);
@@ -47,23 +47,23 @@ app.use(cors())
 app.use(express.json())
 
 const authenticateUser = async (req, res, next) => {
-	const accessToken = req.header('Authorization');
+  const accessToken = req.header('Authorization');
 
-	try {
-		const user = await User.findOne({ accessToken });
-		if (user) {
-			next();
-		} else {
-			res.status(401).json({
-				response: {
-					message: 'Please, log in',
-				},
-				success: false,
-			});
-		}
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: {
+          message: 'Please, log in',
+        },
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 };
 
 // Start defining your routes here
@@ -73,45 +73,45 @@ app.get('/', (req, res) => {
 
 app.get('/thoughts', authenticateUser);
 app.get('/thoughts', async (req, res) => {
-	const thoughts = await Thought.find({});
-	res.status(201).json({ response: thoughts, success: true });
+  const thoughts = await Thought.find({});
+  res.status(201).json({ response: thoughts, success: true });
 });
 
 app.post('/thoughts', async (req, res) => {
-	const { message } = req.body;
+  const { message } = req.body;
 
-	try {
-		const newThought = await new Thought({ message }).save();
-		res.status(201).json({ response: newThought, success: true });
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+  try {
+    const newThought = await new Thought({ message }).save();
+    res.status(201).json({ response: newThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 app.post('/signup', async (req, res) => {
-	const { username, password } = req.body;
+  const { username, password } = req.body;
 
-	try {
-		const salt = bcrypt.genSaltSync();
+  try {
+    const salt = bcrypt.genSaltSync();
 
-		if (password.length < 5) {
-			throw { message: 'Password must be at least 5 characters long' };
-		}
+    if (password.length < 5) {
+      throw { message: 'Password must be at least 5 characters long' };
+    }
 
-		const newUser = await new User({
-			username,
-			password: bcrypt.hashSync(password, salt),
-		}).save();
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    }).save();
 
-		res.status(201).json({
-			response: {
-				userId: newUser._id,
-				username: newUser.username,
-				accessToken: newUser.accessToken,
-			},
-			success: true,
-		});
-	} catch (error) {
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        accessToken: newUser.accessToken,
+      },
+      success: true,
+    });
+  } catch (error) {
     if (error.code === 11000) {
       if (error.keyValue.username) {
         res.status(400).json({
@@ -121,34 +121,34 @@ app.post('/signup', async (req, res) => {
         });
       } 
     }
-		res.status(400).json({ response: error, success: false });
-	}
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 app.post('/signin', async (req, res) => {
-	const { username, password } = req.body;
+  const { username, password } = req.body;
 
-	try {
-		const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username });
 
-		if (user && bcrypt.compareSync(password, user.password)) {
-			res.status(200).json({
-				response: {
-					userId: user._id,
-					username: user.username,
-					accessToken: user.accessToken,
-				},
-				success: true,
-			});
-		} else {
-			res.status(404).json({
-				response: "Username or password doesn't match",
-				success: false,
-			});
-		}
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      });
+    } else {
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Start the server
