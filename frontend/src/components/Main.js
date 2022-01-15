01485e181e35d04a70d9add3d60d55db01ded38a
Darya <darya.lapata@gmail.com>
2022-01-15 09:00:09 +0100

    fixed intendation frontend

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,50 +6,50 @@ import { API_URL } from '../utils/constants';
 import thoughts from '../reducers/thoughts';
 
 const Main = () => {
-	const thoughtsItems = useSelector((store) => store.thoughts.items);
-	const accessToken = useSelector((store) => store.user.accessToken);
+  const thoughtsItems = useSelector((store) => store.thoughts.items);
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		if (!accessToken) {
-			navigate('/login');
-		}
-	}, [accessToken, navigate]);
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/login');
+    }
+  }, [accessToken, navigate]);
 
-	useEffect(() => {
-		const options = {
-			method: 'GET',
-			headers: {
-				Authorization: accessToken,
-			},
-		};
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: accessToken,
+      },
+    };
 
-		fetch(API_URL('thoughts'), options)
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.success) {
-					dispatch(thoughts.actions.setItems(data.response));
-					dispatch(thoughts.actions.setError(null));
-				} else {
-					dispatch(thoughts.actions.setItems([]));
-					dispatch(thoughts.actions.setError(data.response));
-				}
-			});
-	}, [accessToken]);
+    fetch(API_URL('thoughts'), options)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          dispatch(thoughts.actions.setItems(data.response));
+          dispatch(thoughts.actions.setError(null));
+        } else {
+          dispatch(thoughts.actions.setItems([]));
+          dispatch(thoughts.actions.setError(data.response));
+        }
+      });
+  }, [accessToken]);
 
-	return (
-		<div>
-			<div>
-				<Link to="/login">To '/login' !</Link>
-			</div>
-			<h1>Protected happy thoughts:</h1>
-			{thoughtsItems.map((item) => (
-				<div key={item._id}>{item.message}</div>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <Link to="/login">To '/login' !</Link>
+      </div>
+      <h1>Protected happy thoughts:</h1>
+      {thoughtsItems.map((item) => (
+        <div key={item._id}>{item.message}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Main;
