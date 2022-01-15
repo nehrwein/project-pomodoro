7165c4e46848fbdb520ca8edbab6574c2f6aa708
Darya <darya.lapata@gmail.com>
2022-01-15 08:59:14 +0100

    added frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+
+import { Main } from './components/Main';
+import { Login } from './components/Login';
+import { NotFound } from './components/NotFound';
+
+import user from './reducers/user';
+import thoughts from './reducers/thoughts';
+
+const reducer = combineReducers({
+  user: user.reducer,
+  thoughts: thoughts.reducer
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} /> 
+          <Route path="/login" element={<Login />} /> 
+          {/* * new feature in react router for not found */ }
+          <Route path="*" element={<NotFound />} /> 
+        </Routes>
+    </BrowserRouter>
+    </Provider>
   )
 }
---
+++ frontend/src/reducers/user.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'; 
+
+const user = createSlice({
+	name: 'user', 
+	initialState: {
+		userId: null,
+		// here stores already a logged in user 
+		username: null,
+		accessToken: null, 
+		error: null
+	}, 
+	reducers: {
+		setUserId: (store, action) => {
+			store.userId = action.payload; 
+		},
+		setUsername: (store, action) => {
+			store.username = action.payload; 
+		},
+		setAccessToken: (store, action) => {
+			store.accessToken = action.payload; 
+		}, 
+		setError: (store, action) => {
+			store.error = action.payload; 
+		}
+	}
+})
+
+export default user;
