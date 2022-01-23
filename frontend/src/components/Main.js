87bad95236b28801cdf94a85dc9b92f97dff1dc0
Darya <darya.lapata@gmail.com>
2022-01-23 16:20:09 +0100

    added styling main.js

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
+import '@fortawesome/fontawesome-free/js/all.js';
+
 import { API_URL } from "../utils/constants";
 import tasks from "../reducers/tasks";
 import user from "../reducers/user";
@@ -9,6 +11,7 @@ import styled from "styled-components";
 
 const Main = () => {
 	const allTasks = useSelector((store) => store.tasks.items);
+
 	const accessToken = useSelector((store) => store.user.accessToken);
 
 	const userId = useSelector((store) => store.user.userId);
@@ -59,7 +62,7 @@ const Main = () => {
 
 		fetch(API_URL("tasks"), options)
 			.then((res) => res.json())
-			.then((data) => console.log(data))
+			.then((data) => console.log("ADD", data))
 			.finally(() => setTask(""));
 	};
 
@@ -108,40 +111,48 @@ const Main = () => {
 
 		fetch(API_URL(`tasks/${taskId}/complete`), options)
 			.then((res) => res.json())
-			.then((data) => console.log("TOOGLE", data));
+			.then((data) => {
+				console.log(data)
+			});
 	};
 
 	return (
 		<MainContainer>
-			<h1>The main pomodoro site:</h1>
 			<TimerContainer>
-				<h2>Pomodoro timer goes here</h2>
+				Pomodoro timer goes here
 			</TimerContainer>
+			<TaskContainer>
+				{allTasks.map((item) => (
+					<Task key={item._id}>
+						<TextContainer>
+							<input 
+								id='completed'
+								type='checkbox' 
+								onChange={() => onToggleIsComplete(item._id)}
+							/>
+							<label
+								htmlFor='completed'>{item.description}
+							</label>
+						</TextContainer>
+						<ButtonsContainer>
+							{/* Edit/Update feature below needs to be fixed so that user can edit the items. https://ibaslogic.com/how-to-edit-todos-items-in-react/ */}
+							<div onDoubleClick={() => onUpdateTodo(item._id)}><Icon className="fas fa-pen"></Icon></div>
+							<Button onClick={() => onDeleteTodo(item._id)}><Icon className="fa fa-trash" aria-hidden="true"></Icon></Button>
+						</ButtonsContainer>
+					</Task>
+				))}
+				<AddTaskContainer>
+					<Button onClick={onAddTodo}><Icon props="plus" className="fas fa-plus"></Icon></Button>
+					<input
+						id="task"
+						type="text"
+						value={task}
+						placeholder="Add task here..."
+						onChange={(e) => setTask(e.target.value)}
+					/>
+				</AddTaskContainer>
+			</TaskContainer>
 
-			{allTasks.map((item) => (
-				<TaskContainer key={item._id}>
-				<input 
-					id='completed'
-					type='checkbox' 
-					onChange={() => onToggleIsComplete(item._id)}
-				/>
-				  <ToDoLabel
-            completed={item.completed} 
-            htmlFor='completed'>{item.description}
-          </ToDoLabel>
-
-					{/* Edit/Update feature below needs to be fixed so that user can edit the items. https://ibaslogic.com/how-to-edit-todos-items-in-react/ */}
-					<div onDoubleClick={() => onUpdateTodo(item._id)}>Update</div>
-					<button onClick={() => onDeleteTodo(item._id)}>Delete</button>
-				</TaskContainer>
-			))}
-			<input
-				id="task"
-				type="text"
-				value={task}
-				onChange={(e) => setTask(e.target.value)}
-			/>
-			<button onClick={onAddTodo}>Add task</button>
 			<button onClick={() => dispatch(user.actions.setAccessToken(null))}>
 				Log out
 			</button>
@@ -151,30 +162,72 @@ const Main = () => {
 
 export default Main;
 
+
 const MainContainer = styled.div`
-  height: 100%;
+  /*height: auto;
   width: 100%;
   display: flex;
   position: absolute;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 0 auto;
+  margin: 0 auto;*/
 	background: #202D48;
+`;
+
+const TextContainer = styled.div`
+	width:80%;
+	margin-left: 20px;
+	
 `;
 
 const TimerContainer = styled.div`
 	background: linear-gradient(270.42deg, #D75004 0.3%, #8A3403 99.58%);
-
+	width:100%;
+	height: auto;
+	text-align:center;
 `; 
 
 const TaskContainer = styled.div`
-	display: flex; 
-	flex-direction: row; 
+	display:flex;
+	flex-direction: column;
+	width:100%;
+	background-color: #FFF9F5;
 `;
 
-const ToDoLabel = styled.label`
-  text-decoration: ${props => props.completed ? 'line-through' : 'none'};
-`
+const Task = styled.div`
+	display: flex; 
+	flex-direction: row;
+	justify-content: space-between;
+	margin-top: 20px;
+`;
+
+const ButtonsContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+	width: 20%;
+	margin-right: 10px;
+`;
+
+const Icon = styled.i`
+	color: #999696;
+	font-size: 20px;
+`;
+
+const Button = styled.button`
+	border: none; 
+	background-color: #FFF9F5;
+`;
+
+const AddTaskContainer = styled.div`
+	display: flex; 
+	justify-content: center;
+	margin: 20px 0; 
+`; 
+
+//const ToDoLabel = styled.label`
+//  text-decoration: ${props => props.completed ? 'line-through' : 'none'};
+//`
 
 
