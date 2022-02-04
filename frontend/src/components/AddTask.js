2c4647ad70425333637fbb066359c8e1ed20b00b
Rephili <72921631+Rephili@users.noreply.github.com>
2022-02-04 12:54:07 +0100

    fix(Navigation.js): fixed z-index for side menu

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -1,50 +1,55 @@
-import React, { useState }from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { addTodo } from "../reducers/tasks";
-import styled from "styled-components";
-import { Icon } from "styled-components/Styling";
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { addTodo } from "../reducers/tasks"
+import styled from "styled-components"
+import { Icon } from "styled-components/Styling"
 
 const AddTask = () => {
-  const accessToken = useSelector((store) => store.user.accessToken);
-  const userId = useSelector((store) => store.user.userId);
-  const [task, setTask] = useState("");
+  const accessToken = useSelector((store) => store.user.accessToken)
+  const userId = useSelector((store) => store.user.userId)
+  const [task, setTask] = useState("")
   const plusIcon = <FontAwesomeIcon icon={faPlus} />
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const onAddTodo = (accessToken, userId, task) => {
     dispatch(addTodo(accessToken, userId, task))
-    setTask('')
+    setTask("")
   }
-  
+
   return (
     <AddTaskContainer>
-        <AddButton type="submit" onClick={() => onAddTodo(accessToken, userId, task)}><Icon>{plusIcon}</Icon></AddButton>
-        <Input
-          id="task"
-          type="text"
-          value={task}
-          placeholder="Add new task"
-          onChange={(e) => setTask(e.target.value)}
-        />
+      <AddButton
+        type="submit"
+        onClick={() => onAddTodo(accessToken, userId, task)}
+      >
+        <Icon>{plusIcon}</Icon>
+      </AddButton>
+      <Input
+        id="task"
+        type="text"
+        value={task}
+        placeholder="Add new task"
+        onChange={(e) => setTask(e.target.value)}
+      />
     </AddTaskContainer>
-  );
-};
+  )
+}
 
-export default AddTask;
+export default AddTask
 
 const AddTaskContainer = styled.div`
   width: 80%;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-top: 2px solid #D75004;
+  border-top: 2px solid #d75004;
   padding: 20px 0;
-  gap:10px;
+  gap: 10px;
   /*box-sizing: border-box;*/
   /* margin: 10px auto; */
-`; 
+`
 
 const AddButton = styled.button`
   background-color: transparent;
@@ -57,10 +62,10 @@ const Input = styled.input`
   border: none;
   outline: none;
   font-size: 20px;
-  color: #D75004;
+  color: #d75004;
   width: 40%;
 
   ::placeholder {
-    color: #D75004;
+    color: #d75004;
   }
 `
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,27 +1,26 @@
-import React, { useState} from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from 'react-router-dom';
-import { user } from "../reducers/user";
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { Link } from "react-router-dom"
+import { user } from "../reducers/user"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons"
-import styled from 'styled-components';
-
+import styled from "styled-components"
 
 const Navigation = () => {
   const dispatch = useDispatch()
   const MenuIcon = <FontAwesomeIcon icon={faBars} />
   const CloseIcon = <FontAwesomeIcon icon={faTimes} />
-  const [sidebar, setSidebar] = useState(false);
+  const [sidebar, setSidebar] = useState(false)
 
-  const accessToken = useSelector((store) => store.user.accessToken);
+  const accessToken = useSelector((store) => store.user.accessToken)
 
-  const showSidebar = () => setSidebar(!sidebar);
+  const showSidebar = () => setSidebar(!sidebar)
 
   const NavigationLinks = [
-    {title: 'Home', path: '/'}, 
-    {title: 'Settings', path: '/settings'}, 
-    {title: 'Report', path: '/report'},
-    {title: 'About us', path: '/about'},
+    { title: "Home", path: "/" },
+    { title: "Settings", path: "/settings" },
+    { title: "Report", path: "/report" },
+    { title: "About us", path: "/about" },
   ]
 
   const onLogOut = () => {
@@ -34,35 +33,38 @@ const Navigation = () => {
       {accessToken && (
         <>
           <NavBar>
-            <Link to='#'>
+            <Link to="#">
               <Icon onClick={showSidebar}>{MenuIcon}</Icon>
             </Link>
           </NavBar>
           <SideMenu sidebar={sidebar}>
             <StyledUl>
               <StyledLi>
-                <Link to='#'>
-                  <Icon orange onClick={showSidebar}>{CloseIcon}</Icon>
+                <Link to="#">
+                  <Icon orange onClick={showSidebar}>
+                    {CloseIcon}
+                  </Icon>
                 </Link>
               </StyledLi>
               {NavigationLinks.map((item, index) => {
                 return (
                   <NavList key={index}>
-                    <NavLink to={item.path}>
-                      {item.title}
-                    </NavLink>
+                    <NavLink to={item.path}>{item.title}</NavLink>
                   </NavList>
-                );
+                )
               })}
             </StyledUl>
-            <button type='submit' onClick={() => onLogOut()}>Log Out</button>
+            <button type="submit" onClick={() => onLogOut()}>
+              Log Out
+            </button>
           </SideMenu>
-        </>)}
+        </>
+      )}
     </>
-  );
+  )
 }
 
-export default Navigation;
+export default Navigation
 
 const NavBar = styled.div`
   background: linear-gradient(270.42deg, #d75004 0.3%, #8a3403 99.58%);
@@ -75,7 +77,7 @@ const NavBar = styled.div`
 
 const Icon = styled.i`
   margin-right: 15px;
-  color: ${props => props.orange ? '#D75004' : 'white'};
+  color: ${(props) => (props.orange ? "#D75004" : "white")};
   font-size: 28px;
   background: none;
 `
@@ -89,9 +91,9 @@ const SideMenu = styled.nav`
   justify-content: space-between;
   position: fixed;
   top: 0;
-  right: ${props => props.sidebar ? 0 : '-100%'};
-  transition: ${props => props.sidebar ? '350ms' : '850ms'};
-  z-index: 1;
+  right: ${(props) => (props.sidebar ? 0 : "-100%")};
+  transition: ${(props) => (props.sidebar ? "350ms" : "850ms")};
+  z-index: 3;
 `
 
 const NavList = styled.li`
@@ -105,7 +107,7 @@ const NavList = styled.li`
 
 const NavLink = styled(Link)`
   text-decoration: none;
-  color: #D75004;
+  color: #d75004;
   font-size: 18px;
   width: 95%;
   height: 100%;
@@ -115,7 +117,7 @@ const NavLink = styled(Link)`
   border-radius: 4px;
 
   :hover {
-    background-color: #D75004;
+    background-color: #d75004;
     color: white;
   }
 `
